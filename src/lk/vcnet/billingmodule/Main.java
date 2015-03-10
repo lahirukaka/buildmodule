af9d3eee142ce5da1670c99099f6d90b2c79c76e
lahirukaka <lahirukaka@gmail.com>
2015-03-10 22:18:29 +0530

    First commit

---
+++ src/lk/vcnet/billingmodule/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lk.vcnet.billingmodule;
+
+/**
+ *
+ * @author Lahiru Udana <lahirukaka@gmail.com>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/lk/vcnet/billingmodule/ObjectFile.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lk.vcnet.billingmodule;
+
+/**
+ *
+ * @author Lahiru Udana <lahirukaka@gmail.com>
+ */
+public class ObjectFile {
+
+    public ObjectFile() {
+    }
+
+    
+}
